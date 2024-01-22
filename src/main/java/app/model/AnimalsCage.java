c2a55f626fdc24d0cbb4894746b80cf3585c0788
NikitaKolm <nkolmykov7@gmail.com>
2024-01-22 14:39:31 +0300

    Add 1. Class Dog (extends Animal) 2. Bean dog 3. Bean timer 4. DI for AnimalsCage (dogBean, timerBean)

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,33 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("animalCageBean")
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dogBean")
     private Animal animal;
+
+    private Timer timer;
+
+    @Autowired
+    @Qualifier("timerBean")
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
